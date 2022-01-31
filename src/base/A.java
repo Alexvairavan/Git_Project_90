a77ab828fc23a49c08fc391c07b755fc5d61a356
choco <chocobuzz98@gmail.com>
2022-01-31 23:29:56 +0530

    first commit

---
+++ src/base/A.java
@@ -0,0 +1,20 @@
+package base;
+
+public class A {
+	
+	
+	private void method01() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void method02() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
